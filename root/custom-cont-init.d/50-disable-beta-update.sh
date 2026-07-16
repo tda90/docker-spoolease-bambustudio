6affc27af635fa65f7db18d2b35aafb1c78a7e69
tda90 <48199685+tda90@users.noreply.github.com>
2026-07-16 04:37:43 +0200

    Update 50-disable-beta-update.sh

--- root/custom-cont-init.d/50-disable-beta-update.sh
+++ root/custom-cont-init.d/50-disable-beta-update.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/with-contenv bash
-   CONF="/config/.config/BambuStudio/BambuStudio.conf"
-   (
-     while true; do
-       if [ -f "$CONF" ] && [ "$(jq -r '.app.enable_beta_version_update // false' "$CONF")" = "true" ]; then
-         tmp=$(mktemp)
-         jq '.app.enable_beta_version_update = false' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
-         chown abc:abc "$CONF"
-       fi
-       sleep 30
-     done
-   ) &
+CONF="/config/.config/BambuStudio/BambuStudio.conf"
+
+disable_beta() {
+  if [ -f "$CONF" ] && [ "$(jq -r '.app.enable_beta_version_update // false' "$CONF")" = "true" ]; then
+    tmp=$(mktemp)
+    jq '.app.enable_beta_version_update = false' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
+    chown abc:abc "$CONF"
+  fi
+}
+
+(
+  # Aggressive for the first ~2 minutes to catch the first-run config write fast,
+  # then relax to a light background check.
+  for i in $(seq 1 120); do
+    disable_beta
+    sleep 1
+  done
+  while true; do
+    disable_beta
+    sleep 30
+  done
+) &
